a0932cd98928091fc15cd8eb5daa7e83c5a8de12
frenchfritzy <fritz32973@gmail.com>
2025-11-08 16:04:35 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("proceed");
+    const alert = document.getElementById("alert");
+    
+
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const answer = document.getElementById("answer").value;
+
+        if (!answer) {
+            alert.textContent = "ANSWER THE QUESTION TO PROCEED";
+            alert.style.color = "blue";
+            alert.classList.add("show");
+            setTimeout(() => {
+                alert.classList.remove("show");
+            }, 3000);
+            return;
+        }
+
+        if (answer === "Nizza") {
+            window.location.href = "home.html";
+        } else {
+            alert.textContent = "MALIIIIIIIIIIII!"
+            alert.style.color = "red";
+            alert.classList.add("show");
+            setTimeout(() => {
+                alert.classList.remove("show");
+            }, 3000);
+        }
+    });
